8739739095451051cd586a9c65ac701247b65360
Priscilla Nascimento <priscillans2712@gmail.com>
2018-05-18 21:42:48 -0300

    Removed drugstore references

--- app/src/androidTest/java/com/example/priscilla/nearbyplaces/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/priscilla/nearbyplaces/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.priscilla.drugstore", appContext.packageName)
+        assertEquals("com.example.priscilla.nearbyplaces", appContext.packageName)
     }
 }
