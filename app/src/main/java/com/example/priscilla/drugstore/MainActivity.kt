5f1040f86c01a9733728fbb58d6055e50d6d2a13
Priscilla Nascimento <priscillans2712@gmail.com>
2018-05-18 21:34:43 -0300

    Added missing import

--- app/src/main/java/com/example/priscilla/drugstore/MainActivity.kt
+++ app/src/main/java/com/example/priscilla/drugstore/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.priscilla.drugstore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.priscilla.nearbyplaces.R
 
 class MainActivity : AppCompatActivity() {
 
